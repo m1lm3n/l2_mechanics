6b8fb7162fd85e1602f288b9c1567ff51f19f6e4
M1lm3n <m1lm3n@tuta.io>
2024-07-04 18:03:58 +0300

    Init

---
+++ js/core.js
@@ -0,0 +1,25 @@
+function nav_btn_1() {
+    var element = document.getElementById("nav-1");
+    element.classList.toggle("open");
+};
+
+function nav_btn_2() {
+    var element = document.getElementById("nav-2");
+    element.classList.toggle("open");
+
+};
+
+function nav_btn_3() {
+    var element = document.getElementById("nav-3");
+    element.classList.toggle("open");
+};
+
+function nav_btn_4() {
+    var element = document.getElementById("nav-4");
+    element.classList.toggle("open");
+};
+
+function nav_btn_5() {
+    var element = document.getElementById("nav-5");
+    element.classList.toggle("open");
+};
