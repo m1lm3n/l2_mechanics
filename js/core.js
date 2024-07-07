8f21ef8d1f9535f328756ddb767ae1cc7da6c68a
M1lm3n <m1lm3n@tuta.io>
2024-07-07 10:25:20 +0300

    Completed markup of nav menu:

--- js/core.js
+++ js/core.js
@@ -56,3 +56,22 @@ function btn_exit(element) {
 
     console.log('Event triggered');
 };
+
+jQuery(document).ready(function($){
+	var isLateralNavAnimating = false;
+	
+	//open/close lateral navigation
+	$('.cd-nav-trigger').on('click', function(event){
+		event.preventDefault();
+		//stop if nav animation is running 
+		if( !isLateralNavAnimating ) {
+			if($(this).parents('.csstransitions').length > 0 ) isLateralNavAnimating = true; 
+			
+			$('body').toggleClass('navigation-is-open');
+			$('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				//animation is over
+				isLateralNavAnimating = false;
+			});
+		}
+	});
+});
