0e6ec28cac6a9b6be6376882b8a7cf2f1d2998d8
M1lm3n <m1lm3n@tuta.io>
2024-08-07 18:29:27 +0300

    Added Corupted Man

--- js/addon_summon.js
+++ js/addon_summon.js
@@ -4,13 +4,27 @@ jQuery(document).ready(function ($) {    // Optimization: Store the references o
     var element1 = document.getElementById("nav-1-1");
     element1.classList.toggle("open");
 
-    var coll = document.getElementsByClassName("collapsible");
+    var coll = document.getElementsByClassName("collap");
     var i;
 
     for (i = 0; i < coll.length; i++) {
         coll[i].addEventListener("click", function () {
             this.classList.toggle("active");
             var content = document.getElementById("collapsible-content");
+            if (content.style.maxHeight){
+                content.style.maxHeight = null;
+              } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+              } 
+        });
+    }
+
+    var coll = document.getElementsByClassName("collap1");
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = document.getElementById("collapsible-content1");
             if (content.style.maxHeight){
                 content.style.maxHeight = null;
               } else {
