8cefeb2b987509decfeb4cfbda91a851b434f009
M1lm3n <m1lm3n@tuta.io>
2024-08-15 13:04:42 +0300

    Abbys Walker

--- js/addon_skillenchant.js
+++ js/addon_skillenchant.js
@@ -1,7 +1,23 @@
-$(document).ready(function() {
+jQuery(document).ready(function ($) {    // Optimization: Store the references outside the event handler:
     // Optimization: Store the references outside the event handler:
     var element = document.getElementById("nav-1");
     element.classList.toggle("open");
     var element1 = document.getElementById("nav-1-1");
     element1.classList.toggle("open");
+
+    var i;
+
+    var coll = document.getElementsByClassName("collap1");
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = document.getElementById("collapsible-content1");
+            if (content.style.maxHeight){
+                content.style.maxHeight = null;
+              } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+              } 
+        });
+    }
 });
