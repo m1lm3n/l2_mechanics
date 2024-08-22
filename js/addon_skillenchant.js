2ee67e02c231c0906b960977d3ee3edecef70f7b
RAZER-BLADE\HOME <m1lm3n@tuta.io>
2024-08-22 17:24:11 +0300

    Added ES

--- js/addon_skillenchant.js
+++ js/addon_skillenchant.js
@@ -86,4 +86,18 @@ jQuery(document).ready(function ($) {    // Optimization: Store the references o
               } 
         });
     }
+    var coll = document.getElementsByClassName("collap7");
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = document.getElementById("collapsible-content7");
+            if (content.style.maxHeight){
+                content.style.maxHeight = null;
+              } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+              } 
+        });
+    }
+
 });
