8a299b15cc8804564222a0d32396417553d43200
RAZER-BLADE\HOME <m1lm3n@tuta.io>
2024-08-23 07:34:03 +0300

    Added EE

--- js/addon_skillenchant.js
+++ js/addon_skillenchant.js
@@ -99,5 +99,17 @@ jQuery(document).ready(function ($) {    // Optimization: Store the references o
               } 
         });
     }
+    var coll = document.getElementsByClassName("collap8");
 
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = document.getElementById("collapsible-content8");
+            if (content.style.maxHeight){
+                content.style.maxHeight = null;
+              } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+              } 
+        });
+    }
 });
