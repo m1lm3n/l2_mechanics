f282693343cec24c2c2bbad686911d80a735e934
RAZER-BLADE\HOME <m1lm3n@tuta.io>
2024-08-25 18:55:02 +0300

    Added Hawk

--- js/addon_skillenchant.js
+++ js/addon_skillenchant.js
@@ -139,4 +139,18 @@ jQuery(document).ready(function ($) {    // Optimization: Store the references o
               } 
         });
     }
+
+    var coll = document.getElementsByClassName("collap11");
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = document.getElementById("collapsible-content11");
+            if (content.style.maxHeight){
+                content.style.maxHeight = null;
+              } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+              } 
+        });
+    }
 });
