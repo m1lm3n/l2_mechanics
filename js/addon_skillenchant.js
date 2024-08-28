f14b426e67afe28e67f8c6fbd0748df9465d0158
RAZER-BLADE\HOME <m1lm3n@tuta.io>
2024-08-28 16:33:26 +0300

    Added Overlord

--- js/addon_skillenchant.js
+++ js/addon_skillenchant.js
@@ -153,4 +153,18 @@ jQuery(document).ready(function ($) {    // Optimization: Store the references o
               } 
         });
     }
+
+    var coll = document.getElementsByClassName("collap12");
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = document.getElementById("collapsible-content12");
+            if (content.style.maxHeight){
+                content.style.maxHeight = null;
+              } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+              } 
+        });
+    }
 });
