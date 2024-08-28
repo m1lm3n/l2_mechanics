390923cc6e0ed2448d7d2948e143a8f301865a89
RAZER-BLADE\HOME <m1lm3n@tuta.io>
2024-08-28 17:53:11 +0300

    Added Paladin

--- js/addon_skillenchant.js
+++ js/addon_skillenchant.js
@@ -167,4 +167,18 @@ jQuery(document).ready(function ($) {    // Optimization: Store the references o
               } 
         });
     }
+
+    var coll = document.getElementsByClassName("collap13");
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = document.getElementById("collapsible-content13");
+            if (content.style.maxHeight){
+                content.style.maxHeight = null;
+              } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+              } 
+        });
+    }
 });
