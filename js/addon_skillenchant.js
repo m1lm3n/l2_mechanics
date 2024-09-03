7c300bba2e093b1a203deacbb63a1f2559926633
RAZER-BLADE\HOME <m1lm3n@tuta.io>
2024-09-03 15:03:58 +0300

    Added PW

--- js/addon_skillenchant.js
+++ js/addon_skillenchant.js
@@ -263,4 +263,18 @@ jQuery(document).ready(function ($) {    // Optimization: Store the references o
               } 
         });
     }
+
+    var coll = document.getElementsByClassName("collap20");
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = document.getElementById("collapsible-content20");
+            if (content.style.maxHeight){
+                content.style.maxHeight = null;
+              } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+              } 
+        });
+    }
 });
