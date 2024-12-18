bdac383570688f54c2075b52e94a904160e62f32
sasha <m3lman.94@gmail.com>
2024-12-18 20:12:14 +0200

    Added Debuff page

---
+++ js/addon_debuff.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    // Optimization: Store the references outside the event handler:
+    var element = document.getElementById("nav-4");
+    element.classList.toggle("open");
+    var element1 = document.getElementById("nav-4-4");
+    element1.classList.toggle("open");
+});
