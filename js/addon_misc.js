541f07629d2908d8b4118e97e63eaacec5b0dce4
sasha <m3lman.94@gmail.com>
2024-12-19 11:55:20 +0200

    Added Attributes

---
+++ js/addon_misc.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    // Optimization: Store the references outside the event handler:
+    var element = document.getElementById("nav-5");
+    element.classList.toggle("open");
+    var element1 = document.getElementById("nav-5-5");
+    element1.classList.toggle("open");
+});
